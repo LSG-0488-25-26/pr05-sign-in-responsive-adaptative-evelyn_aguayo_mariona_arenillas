54215b567d80cc818c4ebb3c21ac39a3ebac68ea
LynAguayo <lyn.aguayoc@gmail.com>
2025-12-28 23:20:06 +0100

    feature - define colors in Color

--- app/src/main/java/com/example/responsiveadaptative/ui/theme/Color.kt
+++ app/src/main/java/com/example/responsiveadaptative/ui/theme/Color.kt
@@ -2,10 +2,38 @@ package com.example.responsiveadaptative.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Purple
+val MoradoPrincipal = Color(0xFF6B4EE6)
+val MoradoClaro = Color(0xFF9D8AEE)
+val MoradoOscuro = Color(0xFF4A35A3)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Gold
+val DoradoAcento = Color(0xFFFFD700)
+val DoradoClaro = Color(0xFFFFE44D)
+
+// Grey
+val GrisOscuro = Color(0xFF1A1A2E)
+val GrisMedio = Color(0xFF2D2D44)
+val GrisClaro = Color(0xFFE8E8E8)
+
+val Blanco = Color(0xFFFFFFFF)
+val Negro = Color(0xFF000000)
+
+val RojoError = Color(0xFFE53935)
+val VerdeExito = Color(0xFF43A047)
+
+// Colors per al theme light
+val light_primary = MoradoPrincipal
+val light_onPrimary = Blanco
+val light_background = Color(0xFFFFFBFF)
+val light_surface = Blanco
+val light_onSurface = Negro
+val light_error = RojoError
+
+// Colors per al theme dark
+val dark_primary = MoradoClaro
+val dark_onPrimary = Negro
+val dark_background = GrisOscuro
+val dark_surface = GrisMedio
+val dark_onSurface = Blanco
+val dark_error = RojoError
