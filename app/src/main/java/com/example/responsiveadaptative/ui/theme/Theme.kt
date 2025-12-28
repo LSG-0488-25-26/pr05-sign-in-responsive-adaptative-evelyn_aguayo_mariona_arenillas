17c25e2b0b27e609edbc510e3e5980e270f6827b
LynAguayo <lyn.aguayoc@gmail.com>
2025-12-28 23:22:01 +0100

    feature - configure theme depending on system config

--- app/src/main/java/com/example/responsiveadaptative/ui/theme/Theme.kt
+++ app/src/main/java/com/example/responsiveadaptative/ui/theme/Theme.kt
@@ -1,55 +1,59 @@
 package com.example.responsiveadaptative.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
+// Esquema de colors per theme dark
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    background = dark_background,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    error = dark_error
 )
 
+// Esquema de colors per theme light
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    background = light_background,
+    surface = light_surface,
+    onSurface = light_onSurface,
+    error = light_error
 )
 
 @Composable
 fun ResponsiveAdaptativeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    // Escollim esquema segons el theme del sistema
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
 
+    // Configurem la barra estat
+    val view = LocalView.current
+    if (view.isInEditMode == false) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
+    // Apliquem el theme
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
